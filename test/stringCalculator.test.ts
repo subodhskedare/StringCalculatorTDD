e373b6c033f5f0c4f7d94087b0a01b9cde398fee
subodh.k <subodh.k@servify.in>
2025-03-14 14:10:34 +0530

    Implement add method and first test case for empty string

--- test/stringCalculator.test.ts
+++ test/stringCalculator.test.ts
@@ -3,10 +3,12 @@ import { SimpleStringCalculator } from "../src/stringCalculator";
 describe("String Calculator", () => {
   let calculator: SimpleStringCalculator;
 
+  //create a new calculator cariable before each test
   beforeEach(() => {
     calculator = new SimpleStringCalculator();
   });
 
+  //test 1 
   test("should return 0 for an empty string", () => {
     expect(calculator.add("")).toBe(0);
   });
